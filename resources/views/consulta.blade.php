40354655bfae3485c751b9663958188bd1c3bd42
Edwin Andrade <eamusic2010@yahoo.com>
2020-12-11 16:42:33 -0500

    maquetado base para la tabla de consulta de empleados

--- resources/views/consulta.blade.php
+++ resources/views/consulta.blade.php
@@ -8,27 +8,62 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">CIDENET</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Consulta <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="#">Registro</a>
       </li>
     </ul>
   </div>
 </nav>
-    <script src="{{asset('js/app.js')}}"></script>
+<div class="container">
+    <div class="row">
+    <table class="table table-bordered table-dark">
+    <thead>
+        <tr>
+        <th scope="col">ID</th>
+        <th scope="col">PRIMER APELLIDO</th>
+        <th scope="col">SEGUNDO APELLIDO</th>
+        <th scope="col">PRIMER NOMBRE</th>
+        <th scope="col">SEGUNDO NOMBRE</th>
+        <th scope="col">PAIS</th>
+        <th scope="col">TIPO DOCUMENTO</th>
+        <th scope="col">NRO DOCUMENTO</th>
+        <th scope="col">EMAIL</th>
+        <th scope="col">FECHA INGRESO</th>
+        <th scope="col">ESTADO</th>
+        <th scope="col">FECHA Registro</th>
+        <th scope="col">Accion</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td>Apellido1</td>
+        <td>Apellido2</td>
+        <td>Nombre1</td>
+        <td>Nombre2</td>
+        <td>Pais</td>
+        <td>Tipo Documento</td>
+        <td>Nro Documento</td>
+        <td>Email</td>
+        <td>Fecha Ingreso</td>
+        <td>Estado</td>
+        <td>Fecha Registro</td>
+        <td>Acciones</td>
+        </tr>
+    </tbody>
+    </table>
+    </div> {{-- .row --}}
+</div>
+
+<script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
